f5980b076060edcb98d4a94f13e31a8c8e3cd960
Thibaut REMY <thibaut.remy@domora.com>
2014-12-09 16:02:29 +0100

    Add small error gestion

--- scpwnr.js
+++ scpwnr.js
@@ -15,30 +15,43 @@ var openTrack = function(pageUrl) {
 
     casper.start(pageUrl);
     casper.then(function() {
+        // Attach event on resource request
         this.on('resource.requested', function(resource) {
-            // casper.echo(/\*api.soundcloud.com\*/.test(resource.url));
             if (/api.soundcloud.com.*stream/i.test(resource.url)) {
                 streamMp3Address = resource.url;
-                this.log('Stream adress: GOTCHA', 'info');
-                this.log('Stream adress: ' + streamMp3Address, 'debug');
+                this.log('*** Stream adress: GOTCHA', 'info');
+                this.log('*** Stream adress: ' + streamMp3Address, 'debug');
             }
         });
+
+        // Click on the play button
+        if (!this.exists('.heroPlayButton')) {
+            this.log('No play button found', 'error');
+            return;
+        }
         this.click('.heroPlayButton');
         this.wait(100);
 
+        // Open the stream MP3 address
+        if (streamMp3Address == '') {
+            this.log('No stream found', 'error');
+            return;
+        }
         this.thenOpen(streamMp3Address, {
             method: 'get',
             headers: {
                 "Accept": "application/json, text/javascript, */*; q=0.01"
             }
         }, function() {
-            mp3Adress = JSON.parse(this.getPageContent()).http_mp3_128_url;
-            this.log('Mp3 adress: GOTCHA', 'info');
-            this.log('Mp3 adress: GOTCHA', 'info');
 
+            // Get the MP3 address
+            mp3Adress = JSON.parse(this.getPageContent()).http_mp3_128_url;
+            this.log('*** Mp3 adress: GOTCHA', 'info');
+            this.log('*** Mp3 adress: ' + mp3Adress, 'info');
+
+            // Download the mp3
             this.then(function() {
                 this.download(mp3Adress, 'sup.mp3');
-                this.log('')
             })
         });
     });
@@ -47,4 +60,4 @@ var openTrack = function(pageUrl) {
     casper.run();
 }
 
-openTrack('https://soundcloud.com/buygore/elephant');
+openTrack('https://soundcloud.com/buygore/elephnt');
