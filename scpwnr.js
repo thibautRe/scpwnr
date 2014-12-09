6d790660f98acca62048328f514fa4297d6de983
Thibaut REMY <thibaut.remy@domora.com>
2014-12-09 17:38:50 +0100

    Changed casperJs open and start locations.

--- scpwnr.js
+++ scpwnr.js
@@ -1,3 +1,7 @@
+var casper = require('casper').create(creationOptions);
+casper.start()
+
+
 var show = function(_object) {
     console.log(JSON.stringify(_object, undefined, 4));
 }
@@ -27,66 +31,70 @@ var getMp3Name = function(title, artist) {
 };
 
 var openTrack = function(pageUrl) {
-    var casper = require('casper').create(creationOptions);
 
-    var streamMp3Address = '';
-    var mp3Adress = '';
-    var titleText = '';
-    var artistText = '';
-
-    casper.start(pageUrl);
     casper.then(function() {
-        // Attach event on resource request
-        this.on('resource.requested', function(resource) {
-            if (/api.soundcloud.com.*stream/i.test(resource.url)) {
-                streamMp3Address = resource.url;
-                this.log('*** Stream adress: GOTCHA', 'info');
-                this.log('*** Stream adress: ' + streamMp3Address, 'debug');
+        var streamMp3Address = '';
+        var mp3Adress = '';
+        var titleText = '';
+        var artistText = '';
+
+        this.thenOpen(pageUrl);
+        this.then(function() {
+            // Attach event on resource request
+            this.on('resource.requested', function(resource) {
+                if (/api.soundcloud.com.*stream/i.test(resource.url)) {
+                    streamMp3Address = resource.url;
+                    this.log('*** Stream adress: GOTCHA', 'info');
+                    this.log('*** Stream adress: ' + streamMp3Address, 'debug');
+                }
+            });
+
+            // Click on the play button
+            if (!this.exists('.heroPlayButton')) {
+                this.log('No play button found', 'error');
+                return;
             }
-        });
+            this.click('.heroPlayButton');
+            this.wait(100);
 
-        // Click on the play button
-        if (!this.exists('.heroPlayButton')) {
-            this.log('No play button found', 'error');
-            return;
-        }
-        this.click('.heroPlayButton');
-        this.wait(100);
-
-        // Retrieve the MP3 informations
-        // Retrive the title
-        if (this.exists('.soundTitle__titleHero')) {
-            titleText = this.getElementInfo('.soundTitle__titleHero').text.trim();
-        }
-        // Retrieve the artist
-        if (this.exists('.soundTitle__usernameHero')) {
-            artistText = this.getElementInfo('.soundTitle__usernameHero').text.trim();
-        }       
-
-        // Open the stream MP3 address
-        if (streamMp3Address == '') {
-            this.log('No stream found', 'error');
-            return;
-        }
-        this.thenOpen(streamMp3Address, {
-            method: 'get',
-            headers: {
-                "Accept": "application/json, text/javascript, */*; q=0.01"
+            // Retrieve the MP3 informations
+            // Retrive the title
+            if (this.exists('.soundTitle__titleHero')) {
+                titleText = this.getElementInfo('.soundTitle__titleHero').text.trim();
             }
-        }, function() {
+            // Retrieve the artist
+            if (this.exists('.soundTitle__usernameHero')) {
+                artistText = this.getElementInfo('.soundTitle__usernameHero').text.trim();
+            }       
 
-            // Get the MP3 address
-            mp3Adress = JSON.parse(this.getPageContent()).http_mp3_128_url;
-            this.log('*** Mp3 adress: GOTCHA', 'info');
-            this.log('*** Mp3 adress: ' + mp3Adress, 'info');
+            // Open the stream MP3 address
+            if (streamMp3Address == '') {
+                this.log('No stream found', 'error');
+                return;
+            }
+            this.thenOpen(streamMp3Address, {
+                method: 'get',
+                headers: {
+                    "Accept": "application/json, text/javascript, */*; q=0.01"
+                }
+            }, function() {
 
-            // Download the mp3
-            this.then(function() {
-                this.download(mp3Adress, getMp3Name(titleText, artistText)+ '.mp3');
-            })
+                // Get the MP3 address
+                mp3Adress = JSON.parse(this.getPageContent()).http_mp3_128_url;
+                this.log('*** Mp3 adress: GOTCHA', 'info');
+                this.log('*** Mp3 adress: ' + mp3Adress, 'info');
+
+                // Download the mp3
+                this.then(function() {
+                    this.download(mp3Adress, getMp3Name(titleText, artistText)+ '.mp3');
+                })
+            });
         });
     });
-    casper.run();
 };
 
-openTrack('https://soundcloud.com/shirobon/shibuya');
+
+openTrack('https://soundcloud.com/firepowerrecs/2-phaseone-touching-the-stars?in=firepowerrecs/sets/phaseone-touching-the-stars');
+openTrack('https://soundcloud.com/your-ol-lady/autodidakt-kroyclub-popo-your-ol-lady-remix');
+
+casper.run();
